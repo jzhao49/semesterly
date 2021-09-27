44c5dfec256984fe7550ca9b7bfe9125f953a1c2
Justin Zhao <jzhao49@jhu.edu>
2021-09-26 22:54:39 -0400

    Added link to Semesterly press page to mock button

--- static/js/redux/ui/modals/user_acquisition_modal.jsx
+++ static/js/redux/ui/modals/user_acquisition_modal.jsx
@@ -117,7 +117,12 @@ class UserAcquisitionModal extends React.Component {
             <span>Continue with Google</span>
           </button>
 
-          <button className="btn abnb-btn secondary">
+          <button className="btn abnb-btn secondary" onClick={() => {
+            const link = document.createElement('a');
+            link.href = 'https://semester.ly/press';
+            document.body.appendChild(link);
+            link.click();
+          }}>
             <span className="img-icon">
               <i className="fa fa-envelope-o" />
             </span>
